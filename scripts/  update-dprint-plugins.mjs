1698161630e2c362d38e004b6d0cefe26935533b
Justin <justin.blair.rankin@gmail.com>
2026-01-11 23:08:16 +0100

    chore: package.json update

--- scripts/  update-dprint-plugins.mjs
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-
-const PLUGINS = {
-  json: "https://plugins.dprint.dev/json-0.21.1.wasm",
-  typescript: "https://plugins.dprint.dev/typescript-0.95.13.wasm",
-  markdown: "https://plugins.dprint.dev/markdown-0.20.0.wasm",
-};

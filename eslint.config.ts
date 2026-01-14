e1b76725ca1d0638bc8010a6169b5d2f908457ef
Justin <justin.blair.rankin@gmail.com>
2026-01-14 09:53:58 +0100

    feat: eslint.config rules adjust

--- eslint.config.ts
+++ eslint.config.ts
@@ -40,9 +40,13 @@ const config: Linter.Config[] = [
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
+          args: 'all',
           argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
+          caughtErrors: 'all',
           caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
         },
       ],
 
